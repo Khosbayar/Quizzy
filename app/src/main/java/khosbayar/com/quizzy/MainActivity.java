08b8624421545848db3e5fbd1310a9a7792ca2be
hosoo1 <kbuyandalai@mum.edu>
2018-07-18 20:49:04 -0500

    Login register UI complete

--- app/src/main/java/khosbayar/com/quizzy/MainActivity.java
+++ app/src/main/java/khosbayar/com/quizzy/MainActivity.java
@@ -2,8 +2,10 @@ package khosbayar.com.quizzy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,10 +15,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView tx = (TextView)findViewById(R.id.tv_appname);
+        TextView tx = (TextView) findViewById(R.id.tv_appname);
 
-        Typeface custom_font = Typeface.createFromAsset(getAssets(),  "fonts/BebasNeue-Regular.ttf");
+        Typeface custom_font = Typeface.createFromAsset(getAssets(), "fonts/BebasNeue-Regular.ttf");
 
         tx.setTypeface(custom_font);
+    }
+
+
+    public void NavToSignUp(View view) {
+        Intent intent = new Intent(getApplicationContext(), SignUpActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/khosbayar/com/quizzy/SignUpActivity.java
@@ -0,0 +1,14 @@
+package khosbayar.com.quizzy;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class SignUpActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+
+}
